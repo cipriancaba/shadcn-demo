c6a5a41ac26f2a1bced52f9ae1c62778e11c538d
Ciprian Caba <caba.ciprian@gmail.com>
2024-07-25 13:03:05 +0300

    Updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,22 @@
 import type { Config } from "tailwindcss"
+import { fontFamily } from "tailwindcss/defaultTheme"
 
-const config = {
-  darkMode: ["class"],
+const config: Config = {
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    {
+      pattern:
+        /bg-(yellow|orange|purple|blue|teal|emerald|cyan|indigo|violet|fuchsia|red|rose|green|amber|slate|sky|pink|lime)-(50|100|500)/,
+    },
+    {
+      pattern:
+        /border-(yellow|orange|purple|blue|teal|emerald|cyan|indigo|violet|fuchsia|red|rose|green|amber|slate|sky|pink|lime)-300/,
+    },
+  ],
   theme: {
     container: {
       center: true,
@@ -18,12 +26,37 @@ const config = {
       },
     },
     extend: {
+      gridTemplateColumns: {
+        24: "repeat(24, minmax(0, 1fr))",
+      },
+      gridColumn: {
+        "span-24": "span 24 / span 24",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        brand: {
+          DEFAULT: "hsl(var(--brand))",
+          foreground: "hsl(var(--brand-foreground))",
+        },
+        brandSecondary: {
+          DEFAULT: "hsl(var(--brand-secondary))",
+          foreground: "hsl(var(--brand-secondary-foreground))",
+        },
+        brandAction: {
+          DEFAULT: "hsl(var(--brand-action))",
+          foreground: "hsl(var(--brand-action))",
+        },
+        brandActionBorder: {
+          DEFAULT: "hsl(var(--brand-action-border))",
+          foreground: "hsl(var(--brand-action-border))",
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -72,9 +105,104 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: ({ theme: _ }: { theme: any }) => ({
+        h1: {
+          css: {
+            fontSize: "var(--typography-h1-font-size)",
+            fontWeight: "var(--typography-h1-font-weight)",
+            lineHeight: "var(--typography-h1-line-height)",
+            leterSpacing: "var(--typography-h1-letter-spacing)",
+          },
+        },
+        h2: {
+          css: {
+            fontSize: "var(--typography-h2-font-size)",
+            fontWeight: "var(--typography-h2-font-weight)",
+            lineHeight: "var(--typography-h2-line-height)",
+            leterSpacing: "var(--typography-h2-letter-spacing)",
+          },
+        },
+        h3: {
+          css: {
+            fontSize: "var(--typography-h3-font-size)",
+            fontWeight: "var(--typography-h3-font-weight)",
+            lineHeight: "var(--typography-h3-line-height)",
+            leterSpacing: "var(--typography-h3-letter-spacing)",
+          },
+        },
+        h4: {
+          css: {
+            fontSize: "var(--typography-h4-font-size)",
+            fontWeight: "var(--typography-h4-font-weight)",
+            lineHeight: "var(--typography-h4-line-height)",
+            leterSpacing: "var(--typography-h4-letter-spacing)",
+          },
+        },
+        large: {
+          css: {
+            fontSize: "var(--typography-h4-font-size)",
+            fontWeight: "var(--typography-h4-font-weight)",
+            lineHeight: "var(--typography-h4-line-height)",
+          },
+        },
+        p: {
+          css: {
+            fontSize: "var(--typography-p-font-size)",
+            fontWeight: "var(--typography-p-font-weight)",
+            lineHeight: "var(--typography-p-line-height)",
+          },
+        },
+        pe: {
+          css: {
+            fontSize: "var(--typography-pe-font-size)",
+            fontWeight: "var(--typography-pe-font-weight)",
+            lineHeight: "var(--typography-pe-line-height)",
+          },
+        },
+        blockquote: {
+          css: {
+            fontSize: "var(--typography-blockquote-font-size)",
+            fontWeight: "var(--typography-blockquote-font-weight)",
+            fontStyle: "var(--typography-blockquote-font-style)",
+            lineHeight: "var(--typography-blockquote-line-height)",
+            leterSpacing: "var(--typography-blockquote-letter-spacing)",
+          },
+        },
+        lead: {
+          css: {
+            fontSize: "var(--typography-lead-font-size)",
+            fontWeight: "var(--typography-lead-font-weight)",
+            lineHeight: "var(--typography-lead-line-height)",
+            leterSpacing: "var(--typography-lead-letter-spacing)",
+          },
+        },
+        small: {
+          css: {
+            fontSize: "var(--typography-small-font-size)",
+            fontWeight: "var(--typography-small-font-weight)",
+            lineHeight: "var(--typography-small-line-height)",
+            leterSpacing: "var(--typography-small-letter-spacing)",
+          },
+        },
+        subtle: {
+          css: {
+            fontSize: "var(--typography-subtle-font-size)",
+            fontWeight: "var(--typography-subtle-font-weight)",
+            lineHeight: "var(--typography-subtle-line-height)",
+            color: "var(--typography-subtle-color)",
+          },
+        },
+        detail: {
+          css: {
+            fontSize: "var(--typography-detail-font-size)",
+            fontWeight: "var(--typography-detail-font-weight)",
+            lineHeight: "var(--typography-detail-line-height)",
+          },
+        },
+      }),
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [],
+}
 
 export default config
